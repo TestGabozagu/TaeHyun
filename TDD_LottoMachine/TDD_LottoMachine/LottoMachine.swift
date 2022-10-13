6f84e931da0cab078ae1568f1d35fca781ece46a
taehyunL <taylor1233@naver.com>
2022-10-14 01:04:41 +0900

    로또(자동) 만들기

--- TDD_LottoMachine/TDD_LottoMachine/LottoMachine.swift
+++ TDD_LottoMachine/TDD_LottoMachine/LottoMachine.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+enum LottoMachineError: Error {
+    case invalidNumbers
+    case cannotDivide
+    case lose
+//    case
+}
+
 struct LottoMachine {
     func isValidLottoNumber(of numbers: [Int]) -> Bool {
         guard numbers.count == 6, Set(numbers).count == 6 else {
@@ -24,7 +31,7 @@ struct LottoMachine {
         }
         
         return Array(numberSet)
-    }
+    }//발급 확인
     
     func countingMatchingNumber(user: [Int], winner: [Int]) throws -> Int {
         guard isValidLottoNumber(of: user) && isValidLottoNumber(of: winner) else {
@@ -33,10 +40,39 @@ struct LottoMachine {
         
         let winNumbers = user.filter { winner.contains($0) }
         return winNumbers.count
+    }//중복 확인
+    
+    // MARK: - 로또(자동)
+    
+    func buyLotto(_ cost: Int) throws -> Int {
+        
+        if cost < 1000 {
+            throw LottoMachineError.invalidNumbers
+        }
+        
+        if cost % 1000 != 0 {
+            throw LottoMachineError.cannotDivide
+        }
+        
+        return cost/1000
+        
+    }//구입 가능 확인
+    
+    func winStatistics(user: [Int], winner: [Int]) throws -> [Int] {
+        
+        do {
+            let win = try countingMatchingNumber(user: user, winner: winner)
+            if win >= 3 {
+                return [win, win*10000]
+            } else {
+                throw LottoMachineError.lose
+            }
+        } catch {
+            throw LottoMachineError.invalidNumbers
+        }
     }
+    
 }
 
-enum LottoMachineError: Error {
-    case invalidNumbers
-}
+//6, 5, 4, 3
 
--- TDD_LottoMachine/TDD_LottoMachineTests/TDD_LottoMachineTests.swift
+++ TDD_LottoMachine/TDD_LottoMachineTests/TDD_LottoMachineTests.swift
@@ -9,13 +9,38 @@ import XCTest
 @testable import TDD_LottoMachine
 
 class TDD_LottoMachineTests: XCTestCase {
-
+    var sut: LottoMachine!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = LottoMachine()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
     }
 
+    func test_중복되는수확인가능한지() throws {
+        XCTAssertTrue(sut.isValidLottoNumber(of: [1,2,3,4,5,6]))
+    }
+    
+    func test_1000원미만이면에러를던질까요() throws {
+        XCTAssertThrowsError(try sut.buyLotto(999))
+    }
+    
+    func test_구입가능갯수를맞출수있을지() throws {
+        XCTAssertEqual(try sut.buyLotto(1000), 1)
+    }
+    
+    func test_3개미만이면승자가이니니에러를던질까() throws {
+        XCTAssertThrowsError(try sut.winStatistics(user: [1,2,3,4,5,6], winner: [6,7,8,9,10,11]))
+    }
+    
+    func test_1000단위로안떨어지면에러를잘던질까() throws {
+        XCTAssertThrowsError(try sut.buyLotto(1234))
+    }
+    
 }
